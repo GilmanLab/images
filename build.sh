87976a1172d05720a0328340479cf85cf5a11d0e
Joshua Gilman <joshuagilman@gmail.com>
2020-06-10 15:41:08 -0700

    Initial commit

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "Building base Ubuntu 18.04.4 image..."
+packer build -var-file=vars.common.json -var-file=ubuntu18044/vars.json -force -on-error=ask ubuntu18044/build.json
+
+echo "Building base Kubernetes image..."
+packer build -var-file=vars.common.json -var-file=kube/vars.json -force -on-error=ask kube/build.json
